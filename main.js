c23c85e40ec5f7504f6af5cec3311e3b6666c5ab
Debikapaul <82105390+Debikapaul@users.noreply.github.com>
2021-11-08 20:40:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+song1="";
+song2="";
+
+function preload(){
+  song1=loadSound("song_1.mp3");
+  song2=loadSound("song_2.mp3")
+}
+
+function setup() {
+    canvas = createCanvas(400, 300);
+    canvas.center();
+    
+    video = createCapture(VIDEO);
+    video.hide();
+  }
+
+  function draw(){
+    image(video,0,0,600,500);
+}
+  
